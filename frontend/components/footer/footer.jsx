2fa4e22377939feaec1d38d027a64612e9b7adfb
Ashe <ashe@nyu.edu>
2020-08-05 15:51:30 -0700

    fixing styles and footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => (
     <footer>
         <div className="links">
             <a href="https://github.com/ashehwang/">Github</a>
-            <a href="#">LinkeIn</a>
-            <a href="#">Angelist</a>
+            <a href="https://www.linkedin.com/in/ashe-hyerin-hwang-ba1814104/">LinkedIn</a>
+            <a href="https://angel.co/u/ashe-hwang">AngelList</a>
         </div>
         functionbook &copy; 2020 by Ashe HyeRin Hwang
     </footer>
